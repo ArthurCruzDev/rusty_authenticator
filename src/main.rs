e242abcd7ccbece6d9feaa4bb556605cf1d0245c
DevArthurCruz <developer.arthur.cruz@gmail.com>
2023-03-30 22:54:18 -0300

    FEAT[Server Initialization]: Setup logging and some enviroment variables
    - Added log and log4rus dependencies
    - Added a initialization message for the server/service
    - Implemented host and port variables being parsed from enviroment or set by defaults

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::env;
+
 use actix_web::{
     get, http::header::ContentType, post, web, App, HttpResponse, HttpServer, Responder,
 };
+use log::info;
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
@@ -30,13 +33,26 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+
+    let host: String = env::var("RUSTYAUTH_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port: u16 = env::var("RUSTYAUTH_PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse::<u16>()
+        .unwrap_or_else(|_| panic!("Failed to parse server port number"));
+
+    info!(
+        "Starting Rusty Authenticator Microservice on: {}:{}",
+        host, port
+    );
+
     HttpServer::new(|| {
         App::new()
             .service(healthcheck)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((host, port))?
     .run()
     .await
 }
